459e57c6c839c3edb0e1d341ff87f4605bf6fb2a
liheishai-oss <liheishai@gmail.com>
2025-10-16 11:49:45 +0800

    1

--- assets/edit-DRYeh9Um.js
+++
@@ -1,2 +0,0 @@
-import{_ as o}from"./edit.vue_vue_type_style_index_0_lang-CCCWJcFO.js";import"./index-BwQzQhYQ.js";/* empty css                     *//* empty css               *//* empty css                    *//* empty css                 *//* empty css             */export{o as default};
-//# sourceMappingURL=edit-DRYeh9Um.js.map
--- assets/merchant-AuTetK73.js
+++
@@ -1,2 +0,0 @@
-import{s as r}from"./index-BwQzQhYQ.js";const o=t=>r({url:`/api/v1/admin/merchant/${t}/products`,method:"get"}),s=(t,e={})=>r({url:t,method:"post",data:e}),n=(t,e={})=>r({url:t,method:"post",data:e}),c=(t,e)=>r({url:`${t}/${e}`,method:"get"});export{s as a,o as b,n as e,c as g};
-//# sourceMappingURL=merchant-AuTetK73.js.map
